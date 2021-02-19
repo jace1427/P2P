5febb72db79c3349cdaefd7b2a9b568c0189f33f
Justin <jspidell@uoregon.edu>
2021-02-18 23:52:40 -0800

    A very rough draft of Evan's design

--- flask_p2p.py
+++ flask_p2p.py
@@ -1,16 +1,42 @@
-"""
+"""Flask Backend for the Peer-2-Peer Messaging System.
 
+Functions:
+
+    index
+
+Author:
+
+    Justin Spidell
+
+TODO:
+
+    Add Images (generally spruce up the website)
+    Make 404 better
 """
 import flask
 
 app = flask.Flask(__name__)
+app.secret_key = bytes(1)
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    app.logger.debug("main page entry")
-    return flask.render_template('main.html')
+    app.logger.debug("Main page entry")
+    return flask.render_template("p2p.html")
+
+
+@app.route("/message")
+def message():
+    app.logger.debug("Message entry")
+    return flask.render_template("message.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    flask.session['linkback'] = flask.url_for("index")
+    return flask.render_template('404.html'), 404
 
 
 if __name__ == '__main__':
